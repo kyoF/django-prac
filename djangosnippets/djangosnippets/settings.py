f362c76e95e4e995caa162edab53ce2fa5bff9ce
kyoF <fujiki.kyosuke@gmail.com>
2022-10-13 19:18:51 +0900

    fix: SECRET_KEYが漏れていたので再生成し、見えないように対応

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#j*yt9t$f6g@mzx66=h_0lf+!lo8xs%7hfni-z81xwf_rd*2j'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
